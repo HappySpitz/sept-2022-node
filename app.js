03e6687d24aa7171de25be882b8af9ac094687b5
HappySpitz <tatarinkristina2002@gmail.com>
2023-02-28 18:51:31 +0200

    homework1

--- app.js
+++ app.js
@@ -1,18 +1,59 @@
-// console.log('hello world');
+const path = require('path');
+const fs = require('fs');
 
-// -------------------------------------------
+// fs.mkdir(path.join('main_folder'), (err) => {
+//     if (err) throw new Error(err.message)
+// });
 
-// console.log(__dirname);
-// console.log(__filename);
-// console.log(process.cwd());
+const arrFolder = ['folder1', 'folder2', 'folder3', 'folder4', 'folder5'];
+//
+// for (const name of arrFolder) {
+//     fs.mkdir(path.join('main_folder', name), (err) => {
+//         if (err) throw new Error(err.message)
+//     })
+// }
 
-// --------------------------------------------
+const arrFile = ['file1', 'file2', 'file3', 'file4', 'file5'];
+//
+// for (const name of arrFile) {
+//     fs.writeFile(path.join('main_folder', name), `hello from ${name}`, (err) => {
+//         if (err) throw new Error(err.message)
+//     })
+// }
 
-// const path = require('path')
-// console.log(path)
+// for (const name of arrFile) {
+//     fs.readFile(path.join('main_folder', name), {encoding: 'utf-8'}, (err, data) => {
+//         if (err) throw new Error(err.message);
+//         console.log(data)
+//     })
+// }
 
-// ---------------------------------------------
+// fs.writeFile(path.join('main_folder', 'folder1', 'file'), 'loading..............', (err) => {
+//         if (err) throw new Error(err.message)
+//     })
 
-// const os = require('os')
-// console.log(os.arch());
-// console.log(os.cpus());
+// for (const name of arrFolder) {
+//     fs.writeFile(path.join('main_folder', name, 'file_1'), 'loading..............', (err) => {
+//         if (err) throw new Error(err.message)
+//     })
+// }
+
+// for (const name of arrFolder) {
+//     fs.readdir(path.join('main_folder', name),(err, data) => {
+//         if (err) throw new Error(err.message)
+//         console.log(data)
+//     })
+// }
+
+// for (const name of arrFolder) {
+//     fs.readdir(path.join('main_folder', name), {withFileTypes: true}, (err, data) => {
+//         if (err) throw new Error(err.message)
+//         data.forEach(file => {
+//             console.log(file.isFile());
+//         })
+//     })
+// }
+
+// fs.unlink(path.join('main_folder', 'folder2', 'file_1'), (err) => {
+//     if (err) throw new Error(err.message)
+// })
