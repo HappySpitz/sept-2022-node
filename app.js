f936902705cb27cfa696840ee7bb0cd1345c5523
HappySpitz <tatarinkristina2002@gmail.com>
2023-03-02 13:29:34 +0200

    streams and events

--- app.js
+++ app.js
@@ -1,18 +1,53 @@
-// console.log('hello world');
+// Events
 
-// -------------------------------------------
+// const event = require('node:events');
+//
+// const eventEmitter = new event();
+//
+// eventEmitter.on('click', (data) => {
+//     console.log(data);
+//     console.log('Click click click')
+// });
+//
+// eventEmitter.emit('click', {name: "Anton"});
+// eventEmitter.emit('click', {name: "Anton"});
+//
+// eventEmitter.once('clickAndDie', () => {
+//     console.log('I\'m gonna die after being called');
+// });
+//
+// console.log(eventEmitter.eventNames());
+//
+// eventEmitter.emit('clickAndDie');
+// eventEmitter.emit('clickAndDie');
+// eventEmitter.emit('clickAndDie');
+// eventEmitter.emit('clickAndDie');
+//
+// console.log(eventEmitter.eventNames());
 
-// console.log(__dirname);
-// console.log(__filename);
-// console.log(process.cwd());
+const fs = require('node:fs');
+const path = require('node:path');
 
-// --------------------------------------------
+//Streams
 
-// const path = require('path')
-// console.log(path)
+// const readStream = fs.createReadStream(path.join('main_folder', 'folder6', 'file'));
+// const writeStream = fs.createWriteStream(path.join('main_folder', 'folder2', 'file'))
 
-// ---------------------------------------------
+// readStream.on('data', (chunk) => {
+//     // console.log('---------');
+//     // console.log(chunk);
+//     // console.log('---------');
+//     writeStream.write(chunk)
+// })
 
-// const os = require('os')
-// console.log(os.arch());
-// console.log(os.cpus());
+// const handleError = () => {
+//     console.error('ERROR!!!');
+//     readStream.destroy();
+//     writeStream.end('Error while reading file')
+// }
+//
+// readStream
+//     .on("error", handleError)
+//     .pipe(writeStream)
+//     .on('error', handleError);
+
